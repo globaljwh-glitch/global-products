fd0003d56dea141193316d340c955dcfef6b9e3c
Raman Kant <ramankant.vashisht@jwhglobal.com>
2026-06-19 18:13:38 +0530

    create css file

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -22,6 +22,7 @@
    <link rel="stylesheet" href="{{ asset('css/slick-theme.css') }}">
    <!-- <link rel="stylesheet" href="css/core.css"> -->
    <link rel="stylesheet" href="{{ asset('css/core.css') }}">
+   <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
