10031edcdb74e339869911a0491bbde33101d4ba
Raman Kant <ramankant.vashisht@jwhglobal.com>
2026-06-22 16:22:55 +0530

    contact admin email fix

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -52,7 +52,8 @@ class ContactController extends Controller
 
         $contact = Contact::create($validated);
 
-        Mail::to(env('ADMIN_EMAIL'))->send(new ContactAdminMail($contact));
+        //Mail::to(env('ADMIN_EMAIL'))->send(new ContactAdminMail($contact));
+        Mail::to(config('mail.admin_email'))->send(new ContactAdminMail($contact));
         Mail::to($contact->email)->send(new ContactUserMail($contact));
 
         return redirect()->back()->with('success', 'Your inquiry has been submitted successfully.');
