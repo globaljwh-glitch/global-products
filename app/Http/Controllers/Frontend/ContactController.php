3f454f4467bfe8e346a1e651699410d20b8a28b2
Raman Kant <ramankant.vashisht@jwhglobal.com>
2026-07-02 19:32:12 +0530

    mail queue add for testing

--- app/Http/Controllers/Frontend/ContactController.php
+++ app/Http/Controllers/Frontend/ContactController.php
@@ -53,11 +53,11 @@ class ContactController extends Controller
         $contact = Contact::create($validated);
 
         //Mail::to(env('ADMIN_EMAIL'))->send(new ContactAdminMail($contact));
-        logger('before first mail');
+        //logger('before first mail');
         //Mail::to(config('mail.admin_email'))->send(new ContactAdminMail($contact));
         Mail::to(config('mail.admin_email'))->queue(new ContactAdminMail($contact));
-        //Mail::to($contact->email)->send(new ContactUserMail($contact));
-        logger('without mail send before redirect');
+        Mail::to($contact->email)->queue(new ContactUserMail($contact));
+        //logger('without mail send before redirect');
         return redirect()->back()->with('success', 'Your inquiry has been submitted successfully.');
     }
 }
