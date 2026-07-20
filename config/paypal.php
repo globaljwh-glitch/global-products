62285c20a170b27568c8939f6b762f6970c2f630
Raman Kant <ramankant.vashisht@jwhglobal.com>
2026-07-20 18:08:57 +0530

    setup paypal client id and secret

--- config/paypal.php
+++ config/paypal.php
@@ -8,8 +8,8 @@
 return [
     'mode' => config('paypal.mode', 'production'), //env('PAYPAL_MODE', 'sandbox'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
     'sandbox' => [
-        'client_id' => config('paypal.client_id'), //env('PAYPAL_SANDBOX_CLIENT_ID', ''),
-        'client_secret' => config('paypal.client_secret'), //env('PAYPAL_SANDBOX_CLIENT_SECRET', ''),
+        'client_id' => env('PAYPAL_SANDBOX_CLIENT_ID', ''), //config('paypal.client_id'), //env('PAYPAL_SANDBOX_CLIENT_ID', ''),
+        'client_secret' => env('PAYPAL_SANDBOX_CLIENT_SECRET', ''), //config('paypal.client_secret'), //env('PAYPAL_SANDBOX_CLIENT_SECRET', ''),
         'app_id' => 'APP-80W284485P519543T', // Sandbox app_id is always this fixed value.
     ],
     'live' => [
