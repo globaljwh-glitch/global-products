27411ba65d0077c56a5d18e108744b987810de2d
Raman Kant <ramankant.vashisht@jwhglobal.com>
2026-07-27 20:29:37 +0530

    limit brand

--- resources/views/frontend/categories/industries.blade.php
+++ resources/views/frontend/categories/industries.blade.php
@@ -1,7 +1,7 @@
 <div class="productCategoriesFilter mt-lg-5 mt-md-4 mt-2">
    <h4 class="text-uppercase">Shop By Industry</h4>
    <ul class="ps-0">
-      @foreach($industries_data as $ind)
+      @foreach($industries_data->take(6) as $ind)
       <li><a class="categories-list-group-item categories-list-group-item-action {{ (!empty($industry->id) && $ind->id == $industry->id) ? 'active fw-bold ' : '' }}" href="{{ route('products.index', ['type' => 'industry', 'slug' => $ind->slug]) }}">
          {{ ucfirst($ind->name) }}
       </a></li>
